08d9df0403c5da0471b1ef7a7c207e4341be38e0
lavi2224 <146219826+lavi2224@users.noreply.github.com>
2023-09-27 21:31:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-#2.2 Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object.
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
-# Define the Player class
 
-class Player:
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-    def play(self):
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-        print("The player is playing cricket.")
+  return indices
 
-# Define the Batsman class, derived from Player
 
-class Batsman(Player):
-
-    def play(self):
-
-        print("The batsman is batting.")
-
-# Define the Bowler class, derived from Player
-
-class Bowler(Player):
-
-    def play(self):
-
-        print("The bowler is bowling.")
-
-# Create objects of Batsman and Bowler classes
-
-batsman = Batsman()
-
-bowler = Bowler()
-
-# Call the play() method for each object
-
-batsman.play()
-
-bowler.play()
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
 
