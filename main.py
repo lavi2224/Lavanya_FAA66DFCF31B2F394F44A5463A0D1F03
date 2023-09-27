54083a39eda9e4363a81d749b6447090ab1bc01e
lavi2224 <146219826+lavi2224@users.noreply.github.com>
2023-09-27 21:29:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,29 @@
-# 2.1 Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.
+# 1.1 Implement a recursive function to calculate the factorial of a given number
 
-class BankAccount:
+def recur_factorial(n):  
 
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+   if n == 1:  
 
-        self.__account_number = account_number
+       return n  
 
-        self.__account_holder_name = account_holder_name
+   else:  
 
-        self.__account_balance = initial_balance
+       return n*recur_factorial(n-1)  
 
- 
+# take input from the user  
 
-    def deposit(self, amount):
+num = int(input("Enter a number: "))  
 
-        if amount > 0:
+# check is the number is negative  
 
-            self.__account_balance += amount
+if num < 0:  
 
-            print(f"Deposited ${amount:.2f} into account {self.__account_number}")
+   print("Sorry, factorial does not exist for negative numbers")  
 
-        else:
+elif num == 0:  
 
-            print("Invalid deposit amount. Please deposit a positive amount.")
+   print("The factorial of 0 is 1")  
 
- 
+else:  
 
-    def withdraw(self, amount):
-
-        if amount > 0:
-
-            if self.__account_balance >= amount:
-
-                self.__account_balance -= amount
-
-                print(f"Withdrew ${amount:.2f} from account {self.__account_number}")
-
-            else:
-
-                print("Insufficient balance. Cannot withdraw.")
-
-        else:
-
-            print("Invalid withdrawal amount. Please withdraw a positive amount.")
-
- 
-
-    def display_balance(self):
-
-        print(f"Account {self.__account_number} balance: ${self.__account_balance:.2f}")
-
- 
-
- 
-
-# Testing the BankAccount class
-
-if __name__ == "__main__":
-
-    # Create a BankAccount instance
-
-    account1 = BankAccount("123456", "John Doe", 1000.0)
-
- 
-
-    # Deposit money
-
-    account1.deposit(500.0)
-
- 
-
-    # Withdraw money
-
-    account1.withdraw(200.0)
-
- 
-
-    # Display balance
-
-    account1.display_balance()
-  
+   print("The factorial of",num,"is",recur_factorial(num))
